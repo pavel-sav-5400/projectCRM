54ce093e4b9007b918b15e5527c14316dadbeeca
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-02-10 20:30:32 +1000

    получил 4 элемента: заголовок, форма, дисконт, input дисконта

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const mainModalTitle = document.querySelector('.modal__title');
+const mainModalForm = document.querySelector('.modal__form');
+const modalCheckBox = document.querySelector('.modal__checkbox');
+const modalInputCheckBox = document.querySelector('.modal__input_discount');
