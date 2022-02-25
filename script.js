c08c785d98450678621ff7c7fec23f858bb3cce8
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-02-25 19:28:02 +1000

    Реализовал добавление товара из формы в таблицу

--- script.js
+++ script.js
@@ -4,73 +4,7 @@
 
 const data =
 [
-  {
-    'id': 253842678,
-    'title': 'Смартфон Xiaomi 11T 8/128GB',
-    'price': 27000,
-    'description': `Смартфон Xiaomi 11T – это представитель флагманской линейки,
-     выпущенной во второй половине 2021 года. И он полностью соответствует такому
-      позиционированию, предоставляя своим обладателям возможность пользоваться
-       отличными камерами, ни в чем себя не ограничивать при запуске игр и других
-        требовательных приложений.`,
-    'category': 'Мобильные телефоны',
-    'discont': false,
-    'count': 3,
-    'units': 'шт',
-    'images': {
-      'small': 'img/smrtxiaomi11t-m.jpg',
-      'big': 'img/smrtxiaomi11t-b.jpg',
-    },
-  },
-  {
-    'id': 296378448,
-    'title': 'Радиоуправляемый автомобиль Cheetan',
-    'price': 4000,
-    'description': 'Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет',
-    'category': 'Игрушки',
-    'discont': 5,
-    'count': 1,
-    'units': 'шт',
-    'images': {
-      'small': 'img/cheetancar-m.jpg',
-      'big': 'img/cheetancar-b.jpg',
-    },
-  },
-  {
-    'id': 215796548,
-    'title': 'ТВ приставка MECOOL KI',
-    'price': 12400,
-    'description': `Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL
-     KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом
-      Amlogic S905D'`,
-    'category': 'ТВ приставки',
-    'discont': 15,
-    'count': 4,
-    'units': 'шт',
-    'images': {
-      'small': 'img/tvboxmecool-m.jpg',
-      'big': 'img/tvboxmecool-b.jpg',
-    },
-  },
-  {
-    'id': 246258248,
-    'title': 'Витая пара PROConnect 01-0043-3-25',
-    'price': 22,
-    'description': `Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами
-     проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный
-      медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм
-       широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить
-        развертывание локальной сети в домашних условиях или на предприятии, объединить все
-         необходимое вам оборудование в единую сеть.`,
-    'category': 'Кабели',
-    'discont': false,
-    'count': 420,
-    'units': 'v',
-    'images': {
-      'small': 'img/lan_proconnect43-3-25.jpg',
-      'big': 'img/lan_proconnect43-3-25-b.jpg',
-    },
-  },
+  
 ];
 
 function getRandomIntInclusive(min, max) {
@@ -78,15 +12,16 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+const uniqId = getRandomIntInclusive(1e8, 9e8);
 
 
 const mainModalTitle = document.querySelector('.modal__title');
-const mainModalForm = document.querySelector('.modal__form');
+const form = document.querySelector('.modal__form');
 const modalCheckBox = document.querySelector('.modal__checkbox');
-const modalInputCheckBox = document.querySelector('.modal__input_discount');
+const modalCheckBoxDiscount = document.querySelector('.modal__input_discount');
 const modalCheckBoxInput = document.querySelector('.modal__input');
 const venderCodeId = document.querySelector('.vendor-code__id');
-
+const discountCount = document.querySelector('.modal__input_discount')
 
 const mainModal = document.querySelector('.overlay');
 const tableBody = document.querySelector('.table__body');
@@ -94,12 +29,15 @@ const btnAddGood = document.querySelector('.panel__add-goods');
 
 mainModal.classList.remove('active');
 
+const addNewProd = prod => {
+data.push(prod);
+}
+
 // кнопка добавить товар
 
 btnAddGood.addEventListener('click', () => {
   mainModal.classList.add('active');
-  venderCodeId.replaceWith(getRandomIntInclusive(1e8, 9e8));
-
+  venderCodeId.replaceWith(uniqId);
 });
 
 // закрытие модального окна
@@ -128,15 +66,26 @@ tableBody.addEventListener('click', e => {
 },
 );
 
+// чекбокс
+
+modalCheckBox.addEventListener('click', e => {
+if (e.target.closest('.modal__checkbox')) {
+  discountCount.toggleAttribute('disabled');
+  if (discountCount.hasAttribute('disabled')) {
+    e.target.reset();
+  }
+}
+})
+
 const createRow = data => tableBody.insertAdjacentHTML('afterbegin',
-    `<tr data-id="${data.id}">
-    <td class="table__cell">${data.id}</td>
-    <td class="table__cell table__cell_left table__cell_name" >${data.title}</td>
+    `<tr data-id="${uniqId}">
+    <td class="table__cell">${uniqId}</td>
+    <td class="table__cell table__cell_left table__cell_name" >${data.name}</td>
     <td class="table__cell table__cell_left">${data.category}</td>
     <td class="table__cell">${data.units}</td>
     <td class="table__cell">${data.count}</td>
     <td class="table__cell">$${data.price}</td>
-    <td class="table__cell">$${(data.count * data.price) - (data.price * data.count * data.discont / 100)}</td>
+    <td class="table__cell">$${(data.count * data.price) - (data.price * data.count * data.discount / 100)}</td>
     <td class="table__cell table__cell_btn-wrapper">          
       <button class="table__btn table__btn_pic"></button>
       <button class="table__btn table__btn_edit"></button>
@@ -145,8 +94,18 @@ const createRow = data => tableBody.insertAdjacentHTML('afterbegin',
   </tr> `,
 );
 
-const renderGoods = data => {
-  const prods = data.map(createRow);
-  return prods;
-};
+const renderGoods = data => data.map(createRow);
 renderGoods(data);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const newProd = Object.fromEntries(formData);
+  console.log('newProd: ', newProd);
+  addNewProd(newProd);
+  tableBody.append(createRow(newProd));
+  console.log(data);
+  mainModal.classList.remove('active');
+  form.reset();
+});
+
