4535eaf2c5a44b36b32c9c7c73ead6c3e13ea48a
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-03-16 17:07:55 +1000

    добавил eslint

--- script.js
+++ script.js
@@ -2,26 +2,82 @@
 /* eslint-disable max-len */
 'use strict';
 
-const data =
-[
-  
+const data = [
+  {
+    'id': 253842678,
+    'name': 'Смартфон Xiaomi 11T 8/128GB',
+    'price': 27000,
+    'description': 'Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.',
+    'category': 'mobile-phone',
+    'discount_count': false,
+    'count': 3,
+    'units': 'шт',
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+      'big': 'img/smrtxiaomi11t-b.jpg',
+    },
+  },
+  {
+    'id': 296378448,
+    'name': 'Радиоуправляемый автомобиль Cheetan',
+    'price': 4000,
+    'description': 'Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет',
+    'category': 'toys',
+    'discount_count': 5,
+    'count': 1,
+    'units': 'шт',
+    'images': {
+      'small': 'img/cheetancar-m.jpg',
+      'big': 'img/cheetancar-b.jpg',
+    },
+  },
+  {
+    'id': 215796548,
+    'name': 'ТВ приставка MECOOL KI',
+    'price': 12400,
+    'description': 'Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D',
+    'category': 'tv-box',
+    'discount_count': 15,
+    'count': 4,
+    'units': 'шт',
+    'images': {
+      'small': 'img/tvboxmecool-m.jpg',
+      'big': 'img/tvboxmecool-b.jpg',
+    },
+  },
+  {
+    'id': 246258248,
+    'name': 'Витая пара PROConnect 01-0043-3-25',
+    'price': 22,
+    'description': 'Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.',
+    'category': 'cables',
+    'discount_count': false,
+    'count': 420,
+    'units': 'v',
+    'images': {
+      'small': 'img/lan_proconnect43-3-25.jpg',
+      'big': 'img/lan_proconnect43-3-25-b.jpg',
+    },
+  },
 ];
 
+// eslint-disable-next-line require-jsdoc
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-};
-const uniqId = getRandomIntInclusive(1e8, 9e8);
+}
 
+const headerTotalPrice = document.querySelector('.crm__total-price');
 
 const mainModalTitle = document.querySelector('.modal__title');
 const form = document.querySelector('.modal__form');
+const modalTotalPrice = document.querySelector('.modal__total-price');
 const modalCheckBox = document.querySelector('.modal__checkbox');
 const modalCheckBoxDiscount = document.querySelector('.modal__input_discount');
 const modalCheckBoxInput = document.querySelector('.modal__input');
-const venderCodeId = document.querySelector('.vendor-code__id');
-const discountCount = document.querySelector('.modal__input_discount')
+const vendorCodeId = document.querySelector('.vendor-code__id');
+const discountCount = document.querySelector('.modal__input_discount');
 
 const mainModal = document.querySelector('.overlay');
 const tableBody = document.querySelector('.table__body');
@@ -29,15 +85,38 @@ const btnAddGood = document.querySelector('.panel__add-goods');
 
 mainModal.classList.remove('active');
 
-const addNewProd = prod => {
-data.push(prod);
-}
+const addNewProd = prod => data.push(prod);
+
+// итоговая стоимость на странице
+
+const getTotalPriceMain = (data) => {
+  const total = data.map(data => (data.count * data.price) - (data.price * data.count * data.discount_count / 100)).reduce((a, b) => a + b);
+  console.log('total: ', total);
+  return total;
+};
+
+
+const getModalPrice = () => (form.price.value * form.count.value) -
+ (form.price.value * form.count.value * form.discount_count.value / 100);
+
+form.price.addEventListener('blur', () => {
+  modalTotalPrice.innerHTML = getModalPrice();
+});
+
+form.count.addEventListener('blur', () => {
+  modalTotalPrice.innerHTML = getModalPrice();
+});
+
+form.discount_count.addEventListener('blur', () => {
+  modalTotalPrice.innerHTML = getModalPrice();
+});
 
 // кнопка добавить товар
 
 btnAddGood.addEventListener('click', () => {
+  const uniqId = getRandomIntInclusive(1e8, 9e8);
   mainModal.classList.add('active');
-  venderCodeId.replaceWith(uniqId);
+  vendorCodeId.innerHTML = (uniqId);
 });
 
 // закрытие модального окна
@@ -59,6 +138,7 @@ tableBody.addEventListener('click', e => {
     data.filter(item => {
       if (+item.id === +itemId) {
         data.splice(data.indexOf(item), 1);
+        headerTotalPrice.innerHTML = getTotalPriceMain(data);
       }
     });
     console.log(data);
@@ -69,23 +149,23 @@ tableBody.addEventListener('click', e => {
 // чекбокс
 
 modalCheckBox.addEventListener('click', e => {
-if (e.target.closest('.modal__checkbox')) {
-  discountCount.toggleAttribute('disabled');
-  if (discountCount.hasAttribute('disabled')) {
-    e.target.reset();
+  if (e.target.closest('.modal__checkbox')) {
+    discountCount.toggleAttribute('disabled');
+    if (discountCount.hasAttribute('disabled')) {
+      form.discount_count.replaceWith('');
+    }
   }
-}
-})
+});
 
 const createRow = data => tableBody.insertAdjacentHTML('afterbegin',
-    `<tr data-id="${uniqId}">
-    <td class="table__cell">${uniqId}</td>
+    `<tr data-id="${data.id}">
+    <td class="table__cell">${data.id}</td>
     <td class="table__cell table__cell_left table__cell_name" >${data.name}</td>
     <td class="table__cell table__cell_left">${data.category}</td>
     <td class="table__cell">${data.units}</td>
     <td class="table__cell">${data.count}</td>
     <td class="table__cell">$${data.price}</td>
-    <td class="table__cell">$${(data.count * data.price) - (data.price * data.count * data.discount / 100)}</td>
+    <td class="table__cell">$${(data.count * data.price) - (data.price * data.count * data.discount_count / 100)}</td>
     <td class="table__cell table__cell_btn-wrapper">          
       <button class="table__btn table__btn_pic"></button>
       <button class="table__btn table__btn_edit"></button>
@@ -106,6 +186,7 @@ form.addEventListener('submit', e => {
   tableBody.append(createRow(newProd));
   console.log(data);
   mainModal.classList.remove('active');
+  console.log('getModalPrice: ', getModalPrice());
   form.reset();
+  headerTotalPrice.innerHTML = getTotalPriceMain(data);
 });
-
